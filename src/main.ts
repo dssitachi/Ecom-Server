d1a144fff9d0e1105a9ec1d516055965b96ade91
Dipesh Shivrame <dipeshss@Dipeshs-MacBook-Air.local>
2023-07-06 16:53:55 +0530

    preflight continue

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    "origin": "https://ecommerce-v2-three.vercel.app",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": true
+    "origin": "https://ecommerce-v2-three.vercel.app"
   });
   await app.listen(3000);
 }
