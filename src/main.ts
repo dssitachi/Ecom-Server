ba817e85e2e51f7a71bfa87db385c42946e886e9
Dipesh Shivrame <dipeshss@Dipeshs-MacBook-Air.local>
2023-07-06 15:51:46 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    "origin": "https://ecommerce-v2-three.vercel.app",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  });
   await app.listen(3000);
 }
 bootstrap();
