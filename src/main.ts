8f11cf469ac7dce9463d7b55f749f6f4973c0ace
Dipesh Shivrame <dipeshss@Dipeshs-MacBook-Air.local>
2023-07-07 13:36:41 +0530

    solving CORS issue

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "'*'",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
